0fc1a8b4ffe54d24cc3119b7753b1bbf358c6d9a
alex <alex.shamarin@gmail.com>
2023-12-18 21:57:35 +0200

    Update fonts.js

--- gulp/tasks/fonts.js
+++ gulp/tasks/fonts.js
@@ -34,46 +34,7 @@ const convertTtfTo = (format) => () => (
 export const ttfToWoff = convertTtfTo("woff");
 export const ttfToWoff2 = convertTtfTo("woff2");
 
-
-
-/*
-export const fontsStyle = () => {
-    const fontsFile = `${app.path.srcFolder}/scss/fonts.scss`;
-  
-    fs.readdir(app.path.build.fonts, (err, fontsFiles) => {
-      if (err) {
-        console.log('Error reading font directory:', err);
-        return;
-      }
-  
-      if (!fs.existsSync(fontsFile)) {
-        fs.writeFile(fontsFile, "", (writeErr) => {
-          if (writeErr) {
-            console.log('Error writing to fonts.scss:', writeErr);
-            return;
-          }
-  
-          let c_fontname;
-          for (let i = 0; i < fontsFiles.length; i++) {
-            let fontFileName = fontsFiles[i].split(".")[0];
-  
-            if (c_fontname !== fontFileName) {
-              fs.appendFile(fontsFile, `@include font("${fontFileName}", "${fontFileName}", "400", "normal");\r\n`, (appendErr) => {
-                if (appendErr) {
-                  console.log('Error appending to fonts.scss:', appendErr);
-                }
-              });
-            }
-            c_fontname = fontFileName;
-          }
-        });
-      } else {
-        console.log('Error: fonts.scss file already exists.');
-      }
-    });
-  };*/
-
-  export const fontsStyle = async () => {
+export const fontsStyle = async () => {
 	try {
 		if (!fs.existsSync('./src/scss/base/_fonts.scss')) {
 			return app.gulp.src(`${app.path.buildFolder}/fonts/*.woff`).pipe(
